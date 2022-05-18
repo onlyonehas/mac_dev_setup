162d89c84df8257ff088c0720e39acd51e9fdebe
Hassan Abbas <hassanabbas93@hotmail.com>
2022-05-18 12:47:19 +0100

    wip: added more applications + info about github setup

--- README.md
+++ README.md
@@ -3,15 +3,28 @@
 install x-code which contains dev env for mac 
 //https://developer.apple.com/download/all/?q=command%20line%20tools
 
-
-
 # How to run the script 
 
-chmod 755 YourScriptName.sh
-./YourScriptName.sh
-
+```
+    chmod 755 setup.sh
+    ./setup.sh
+```
 
 # TO DO: add in `.zshrc`
 plugins=(... z)
 eval "$(starship init zsh)"
 
+# Github machine setup 
+To Generate an SSH key:
+ ssh-keygen
+ cat ~/.ssh/id_rsa.pub | pbcopy
+
+
+If you are on version 2.1.17 or greater, paste the text below to generate a GPG key pair.
+$ gpg --full-generate-key
+
+Use the gpg --list-secret-keys --keyid-format=long command to list the long form of the GPG keys
+$ gpg --list-secret-keys --keyid-format=long
+
+Copy Id after the `sec   4096R/`
+ gpg --armor --export **GPG key ID**,
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Xcode https://developer.apple.com/download/all/?q=command%20line%20tools
+
 # administrator access
 sudo -v
 
@@ -17,23 +18,29 @@ brew update
 
 # ohmyzsh and zsh autosuggestions
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-
 # Mac software update 
-# softwareupdate --all --install --force
+//softwareupdate --all --install --force
 
-
+# essential non-gui apps
 brew install node
 brew install python
 brew install python3
 brew install docker
 brew install starship
 brew install awscli
+brew install redis
+brew install java
+npm i -g np
+
+brew install remotemobprogramming/brew/mob
+
+# upgrade to latest version
+brew upgrade remotemobprogramming/brew/mob
 
 # fonts.
 brew tap bramstein/webfonttools
@@ -41,11 +48,9 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-
 # essential apps - gui
 brew install --cask --appdir="/Applications" alfred
 brew install --cask --appdir="~/Applications" iterm2
-brew install --cask adoptopenjdk/openjdk/adoptopenjdk8
 brew install --cask --appdir="/Applications" google-chrome
 brew install --cask --appdir="/Applications" firefox
 brew install --cask --appdir="/Applications" slack
@@ -55,6 +60,7 @@ brew install --cask --appdir="/Applications" microsoft-outlook
 brew install --cask --appdir="/Applications" visual-studio-code
 brew install --cask --appdir="/Applications" fig
 brew install --cask --appdir="/Applications" 1clipboard
-
+brew install --cask --appdir="/Applications" gpg-suite
+brew install --cask --appdir="/Applications" sourcetree
 
 brew cleanup
