fb028cc4ec4d418361928851c36e9bb8408cf955
Hassan Abbas <hassanabbas93@hotmail.com>
2022-05-26 10:22:28 +0100

    feat: script to install vs extensions programmatically

---
+++ vdcode-extensions.sh
@@ -0,0 +1,20 @@
+# Make sure code is set in the PATH
+
+# Open the Command Palette (Cmd+Shift+P) and 
+# type 'shell command' to find the Shell Command: 
+# Install 'code' command in PATH command
+# https://code.visualstudio.com/docs/setup/mac#:~:text=Launching%20from%20the%20command%20line,code'%20command%20in%20PATH%20command
+
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extensionVisualStudioExptTeam.vscodeintellicode
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension WallabyJs.quokka-vscode
+code --install-extension withfig.fig
+code --install-extension wix.vscode-import-cost
+
+code --install-extension christian-kohler.npm-intellisense --disable-extension
+code --install-extension kamikillerto.vscode-colorize --disable-extension
+code --install-extension TabNine.tabnine-vscode --disable-extension
